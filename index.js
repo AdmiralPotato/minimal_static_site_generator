7761adf25f52a904f08657a1003a1bf7eb435d7f
Admiral Potato <admiral@nuclearpixel.com>
2024-09-24 21:04:22 -0600

    Create destination path based on content config

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import {
   writeFile,
   rm,
 } from 'node:fs/promises';
+import { join, dirname } from 'node:path'
 import markdownit from 'markdown-it'
+
+const outputDir = 'dist';
 
 const md = markdownit();
 
@@ -27,42 +30,43 @@ ${content}
 </html>
 `;
 
-const createPage = (config) => {
-  const output = pageTemplate(
-    config.title,
-    md.render(config.content),
-  );
-  writeFile(config.path, output);
-};
-
-await rm('dist', { force: true, recursive: true });
-
-await Promise.all([
-  'dist/contact',
-  'dist/donate',
-].map(async (path) => {
+const createPage = async (config) => {
+  const prefixedPath = join(outputDir, config.path);
+  const folderPath = dirname(prefixedPath);
   await mkdir(
-    path,
+    folderPath,
     {
       recursive: true,
     },
   )
-}));
+  const output = pageTemplate(
+    config.title,
+    md.render(config.content),
+  );
+  writeFile(prefixedPath, output);
+};
+
+await rm(outputDir, { force: true, recursive: true });
 
 [
   {
     title: 'Home',
     content: '# 801Labs.org Home\n\nThis is where the cool goats go',
-    path: 'dist/index.html'
+    path: 'index.html'
   },
   {
     title: 'Contact',
     content: '# Contact Us!\n\nCome hang out with us on Discord, or at our physical hackerspace!',
-    path: 'dist/contact/index.html'
+    path: 'contact/index.html'
+  },
+  {
+    title: 'Contact GoataClause',
+    content: '# Contact GoataClause!\n\nCome get licked by your favorite yule-goat!',
+    path: 'contact/goataclause/index.html'
   },
   {
     title: 'Donate',
     content: '# Please pay our rent!\n\nYou can even donate in GoatCoin! _Goats do have coins, right?_',
-    path: 'dist/donate/index.html'
+    path: 'donate/index.html'
   },
 ].forEach(createPage)
