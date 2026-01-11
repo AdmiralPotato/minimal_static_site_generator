4e760d10f25fc81fde7ce01165093c51a7f7d36b
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2026-01-10 23:07:13 -0700

    Update styling for header and footer

--- templates/basic.js
+++ templates/basic.js
@@ -11,20 +11,22 @@ export default function (config) {
   </head>
   <body>
     <header>
-      <div id="site-logo"><a href="${basePath}/">Home</a></div>
-      <label id="nav-menu-toggle" for="nav-collapse-button">
-        <input type="checkbox" id="nav-collapse-button" />
-        <span id="nav-menu-icon"></span>
-      </label>
-      <nav id="nav-bar">
-        <ul id="nav-bar-items">
-          <li><a href="${basePath}/about/">About</a></li>
-          <li><a href="${basePath}/blog/">Blog</a></li>
-          <li><a href="${basePath}/get-involved/">Get Involved</a></li>
-          <li><a href="${basePath}/contact/">Contact Us</a></li>
-          <li><a href="${basePath}/donate/" class="nav-donate">Donate &gt;</a></li>
-        </ul>
-      </nav>
+      <div id="header-content">
+        <div id="site-logo"><a href="${basePath}/">Home</a></div>
+        <label id="nav-menu-toggle" for="nav-collapse-button">
+          <input type="checkbox" id="nav-collapse-button" />
+          <span id="nav-menu-icon"></span>
+        </label>
+        <nav id="nav-bar">
+          <ul id="nav-bar-items">
+            <li><a href="${basePath}/about/">About</a></li>
+            <li><a href="${basePath}/blog/">Blog</a></li>
+            <li><a href="${basePath}/get-involved/">Get Involved</a></li>
+            <li><a href="${basePath}/contact/">Contact Us</a></li>
+            <li><a href="${basePath}/donate/" class="nav-donate">Donate &gt;</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
     <div class="container">
       ${content}
@@ -43,33 +45,25 @@ export default function (config) {
         </div>
         <div id="footer-nav-box">
           <div class="footer-content-block">
-            <h3><a class="footer-category-link" href="">ABOUT</a></h3>
-            <ul>
-              <li><a class="footer-subcategory-link" href="">Financial Information</a></li>
-              <li><a class="footer-subcategory-link" href="">News</a></li>
-            </ul>
+            <h3><a class="footer-category-link" href="">ABOUT</a>
+            </h3><span>|</span>
+            <h3><a class="footer-category-link" href="">BLOG</a>
+            </h3><span>|</span>
+            <h3><a class="footer-category-link" href="">GET INVOLVED</a>
+            </h3><span>|</span>
+            <h3><a class="footer-category-link" href="">CONTACT US</a></h3>
           </div>
-          <div class="footer-content-block"><h3><a class="footer-category-link" href="">LEARN</a></h3></div>
-          <div class="footer-content-block">
-            <h3><a class="footer-category-link" href="">GET INVOLVED</a></h3>
-            <ul>
-              <li><a class="footer-subcategory-link" href="">Event</a></li>
-              <li><a class="footer-subcategory-link" href="">Location and Hours</a></li>
-              <li><a class="footer-subcategory-link" href="">Hackercamp</a></li>
-            </ul>
-          </div>
-          <div class="footer-content-block"><h3><a class="footer-category-link" href="">NONPROFIT</a></h3></div>
-          <div class="footer-content-block"><h3><a class="footer-category-link" href="">CONTACT US</a></h3></div>
-        </div>
-        <div id="footer-copywrite-block">
+          <div class="footer-divider"></div>
+        <div id="footer-copy-block">
           <p class="footer-copy-text">© 2024 801Labs.org. All rights reserved.</p>
-          <p class="footer-copy-text"><a href="#" id="footer-donate-link">Donate ></a></p>
           <p class="footer-copy-text">
-            801 Labs hackerspace is a 501(c)(3) that is open to the public! <br />
-            353 East 200 South Suite #201, Salt Lake City, UT 84111
+            801 Labs hackerspace is a 501(c)(3) that is open to the public!
           </p>
           <p class="footer-copy-text">
-            <a class="footer-link" href="#">Code of Conduct</a> | <a class="footer-link" href="#">Terms</a>
+            353 East 200 South Suite #201, Salt Lake City, UT 84111
+          </p>
+          <p class="footer-copy-text bottom-links">
+            <a class="footer-link" href="#">Code of Conduct</a><span>|</span><a class="footer-link" href="#">Terms</a><span>|</span><a class="footer-link" href="#">Donate ></a>
           </p>
         </div>
       </div>
